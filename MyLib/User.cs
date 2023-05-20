66e0e4d47a6dd302c6452372cf20ef0c862e4739
maxefimov <efimov.top.academy@gmail.com>
2023-05-20 16:09:09 +0300

    Добавьте файлы проекта.

---
+++ MyLib/User.cs
@@ -0,0 +1,26 @@
+﻿namespace MyLib
+{
+    public class User
+    {
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public User() : this("Anonim", 0) { }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("User move");
+        }
+    }
+}
---
+++ SystemAssemplyReflectionApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+
+string lib = "MyLib";
+
+Assembly assembly = Assembly.LoadFrom($"{lib}.dll");
+Console.WriteLine(assembly.FullName);
+
+Type[] types = assembly.GetTypes();
+foreach (Type t in types)
+    if(t.Namespace == lib)
+    {
+        Console.WriteLine($"{t.Name} {t.FullName}");
+        //foreach (ConstructorInfo c in t.GetConstructors())
+        //{
+        //    Console.WriteLine($"{c.Name}");
+        //    object[] arg = new object[c.GetParameters().Length];
+        //    int i = 0;
+        //    foreach (ParameterInfo p in c.GetParameters())
+        //    {
+        //        Console.Write($"{p.ParameterType.FullName} {p.Name}: ");
+        //        arg[i++] = Console.ReadLine();
+        //    }
+        //    object? obj = c?.Invoke(arg);
+        //}
+
+
+        ConstructorInfo? constructor = null;
+        constructor = t.GetConstructor(new Type[] { typeof(string), typeof(int) });
+
+        object? obj = constructor?.Invoke(new object[] { "Bob", 22 });
+        Console.WriteLine(obj);
+
+        MethodInfo[] methods = t.GetMethods();
+        foreach (MethodInfo m in methods)
+        {
+            if (m.GetParameters().Length == 0)
+            {
+                Console.WriteLine($"method {m.Name} run:");
+                m.Invoke(obj, new object[] { });
+            }
+                
+        }
+    }
+        
